7c94892b2f5665ec60ae160c0ec754190b2411f3
Rohisha Adke <rohisha@gmail.com>
2017-03-12 22:14:30 -0700

    experiments outline

--- experiments.R
+++ experiments.R
@@ -5,3 +5,16 @@
 # of consumption, then display the results of hypothesis testing on the desired field.
 
 
+
+
+# select factor to assess, impact to assess, way to assess
+# ex: impact of step count on restlessness of sleep
+# factors to assess impact of options: amount slept, step count, distance, amount
+# of exercise, time in heart rate zones, variable you put in yourself (tag specific
+# days/hours/nights/mornings), day(s) of the week, months, seasons
+
+# factors to assess impact on:
+# resting heart rate, time in heart rate zones, amount of time restless, weight
+
+# ways to assess
+# plot one vs other, do a linear regression and anova, histogram? etc.
