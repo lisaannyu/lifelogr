4cc40b0724e6bde68ecf77b3a55cec7e49a6fa1b
Rohisha Adke <rohisha@gmail.com>
2017-03-12 21:50:03 -0700

    addings files for experimentation and recommendation functions

---
+++ experiments.R
@@ -0,0 +1,7 @@
+# 5. Experimentation framework. The package will provide the framework for a user 
+# to easily perform A/B or other testing on him/herself - for example, decide 
+# he/she wants to know if his/her heart rate is higher in a statistically 
+# significant way when alcohol or caffeine is consumed, and so mark each day/time 
+# of consumption, then display the results of hypothesis testing on the desired field.
+
+
---
+++ recommendations.R
@@ -0,0 +1,5 @@
+# 4. Recommendations for improvement. Based on a user's choice of goal, we can 
+# recommend strategies based on their data (for example, if a user walks a lot on 
+# the weekdays but not weekends and is not meeting their target step count, 
+# suggest walks on the weekends, at times of day when they are usually active on 
+# the weekdays according to their heart rate).
