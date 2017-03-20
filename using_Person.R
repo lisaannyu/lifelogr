c7258a6eb44cca849866ddd4e95b43b18494f118
Lisa Ann Yu <lisaann.yu@gmail.com>
2017-03-19 19:12:27 -0700

    Add vignette line

--- using_Person.R
+++ using_Person.R
@@ -21,9 +21,8 @@ RA <- Person$new(fitbit_user_email = "rohisha@gmail.com", fitbit_user_pw = "data
                  group_assignments = list("group_months" = group_months, data.frame(NA)),
                  start_date = "2017-03-11", end_date = "2017-03-12")
 
-# should probably change input to fitbit_user_email
 EX <- Person$new(fitbit_user_email = "rohisha@gmail.com", fitbit_user_pw = "datasamplepw",
-                 # apple_data_file = "apple.csv",
+                 apple_data_file = "apple.csv",
                  user_info = list("name" = "EX", "age" = 29, "gender" = "male"),
                  target_steps = 10000,
                  group_assignments = list(data.frame(NA), data.frame(NA)),
@@ -133,6 +132,9 @@ devtools::use_package("tibble", type = "Imports", pkg = pkgName)
 # devtools::use_testthat() # one time only
 setwd("~/GitHub/stats290-project/lifelogr")
 devtools::test()
+
+# Vignette
+devtools::use_vignette("vignette")
 
 # use command line to do R CMD BUILD lifelogr
 # use command line to do R CMD CHECK lifelogr._0...
